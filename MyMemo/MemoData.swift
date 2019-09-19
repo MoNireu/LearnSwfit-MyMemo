9b1471f3af83106f94dad11fd9f208441e804e21
MoNireu <monireu.dev@gmail.com>
2019-09-19 21:39:31 +0900

    feat: Create data model
    Data Model: MemoData.swift

---
+++ MyMemo/MemoData.swift
@@ -0,0 +1,18 @@
+//
+//  MemoData.swift
+//  MyMemo
+//
+//  Created by MoNireu on 19/09/2019.
+//  Copyright © 2019 monireu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemoData {
+    var memoIndex: Int? // 데이터 식별값
+    var title: String? // 메모 제목
+    var contents: String? // 메모 내용
+    var image: UIImage? // 이미지
+    var regdate: Date? // 날짜
+}
