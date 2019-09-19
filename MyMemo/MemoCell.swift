8148bc0791415019f7d7f42d952c48e84d535f45
MoNireu <monireu.dev@gmail.com>
2019-09-19 20:03:41 +0900

    feat: Create custom class for view controllers
    MemoListVC, MemoCell, MemoForm, MemoCell

---
+++ MyMemo/MemoCell.swift
@@ -0,0 +1,24 @@
+//
+//  MemoCell.swift
+//  MyMemo
+//
+//  Created by MoNireu on 19/09/2019.
+//  Copyright © 2019 monireu. All rights reserved.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
