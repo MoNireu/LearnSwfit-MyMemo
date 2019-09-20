37e40ee8537bb0527a010629eed507bd90a9ed96
MoNireu <monireu.dev@gmail.com>
2019-09-20 17:33:27 +0900

    feat:  Add ImagePicking feature

--- MyMemo/MemoFormVC.swift
+++ MyMemo/MemoFormVC.swift
@@ -17,6 +17,12 @@ class MemoFormVC: UIViewController, UIImagePickerControllerDelegate, UINavigatio
     @IBAction func save(_ sender: Any) {
     }
     @IBAction func pick(_ sender: Any) {
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        self.present(picker, animated: false)
     }
     
     override func viewDidLoad() {
@@ -24,6 +30,16 @@ class MemoFormVC: UIViewController, UIImagePickerControllerDelegate, UINavigatio
 
         // Do any additional setup after loading the view.
     }
+    
+    // 이미지 선택을 완료했을 떄 호출되는 메소드
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // 선택된 이미지를 미리보기에 표시한다.
+        self.preview.image = info[.editedImage] as? UIImage
+        
+        //이미지 피커 컨트롤러를 닫는다.
+        picker.dismiss(animated: false)
+    }
+    
     
 
     /*
